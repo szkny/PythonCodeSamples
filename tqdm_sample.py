cde43c89157ec232bd1ac91ee2acaf4872b6df96
Sohei.Suzuki <suzuki.s1008@gmail.com>
2024-01-21 16:01:52 +0900

    add tqdm sample code

---
+++ tqdm_sample.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 21st 15:57:35 2024
+   @file  : tqdm_sample.py
+   @author: szkny
+   @brief : tqdm sample code
+"""
+from tqdm import tqdm
+import time
+
+for i in tqdm(range(20)):
+    time.sleep(0.1)
+
+bar = tqdm(total=30)
+for i in range(bar.total):
+    bar.update(1)
+    time.sleep(0.1)
