fdc69bd84ae4c05a8ad2d91d6428afd84e59f097
Sohei.Suzuki <suzuki.s1008@gmail.com>
2024-08-10 11:54:26 +0900

    refactor: pandas sample

--- pandas_dataframe_string.py
+++ pandas_dataframe_string.py
@@ -27,18 +27,18 @@ if __name__ == "__main__":
     print(df)
     print()
 
-    df_dtypes = df.dtypes.apply(lambda x: x.name).to_dict()
-    df_dict = df.to_dict(orient="records")
-    df_str = json.dumps({"data": df_dict, "dtypes": df_dtypes})
+    df_dtypes1 = df.dtypes.apply(lambda x: x.name).to_dict()
+    df_dict1 = df.to_dict(orient="records")
+    df_str1 = json.dumps({"data": df_dict1, "dtypes": df_dtypes1})
     print("パターン1")
     print("json.dumpsの文字列:")
-    print(df_str)
-    print(f"サイズ: {sys.getsizeof(df_str)} Bytes")
+    print(df_str1)
+    print(f"サイズ: {sys.getsizeof(df_str1)} Bytes")
     print()
 
-    df_load = json.loads(df_str)
-    df_dtypes = {k: v for k, v in df_load["dtypes"].items()}
-    df_restore1 = pd.DataFrame(df_load["data"]).astype(df_dtypes)
+    df_load1 = json.loads(df_str1)
+    df_dtypes1 = {k: v for k, v in df_load1["dtypes"].items()}
+    df_restore1 = pd.DataFrame(df_load1["data"]).astype(df_dtypes1)
     print("復元されたDataFrame:")
     print(df_restore1)
     print(df_restore1.dtypes)
@@ -46,18 +46,18 @@ if __name__ == "__main__":
     print()
     assert_frame_equal(df, df_restore1)
 
-    df_dtypes = df.dtypes.apply(lambda x: x.name).to_dict()
-    df_dict = df.to_dict(orient="list")
-    df_str = json.dumps({"data": df_dict, "dtypes": df_dtypes})
+    df_dtypes2 = df.dtypes.apply(lambda x: x.name).to_dict()
+    df_dict2 = df.to_dict(orient="list")
+    df_str2 = json.dumps({"data": df_dict2, "dtypes": df_dtypes2})
     print("パターン2")
     print("json.dumpsの文字列:")
-    print(df_str)
-    print(f"サイズ: {sys.getsizeof(df_str)} Bytes")
+    print(df_str2)
+    print(f"サイズ: {sys.getsizeof(df_str2)} Bytes")
     print()
 
-    df_load = json.loads(df_str)
-    df_dtypes = {k: v for k, v in df_load["dtypes"].items()}
-    df_restore2 = pd.DataFrame.from_dict(df_load["data"]).astype(df_dtypes)
+    df_load2 = json.loads(df_str2)
+    df_dtypes2 = {k: v for k, v in df_load2["dtypes"].items()}
+    df_restore2 = pd.DataFrame.from_dict(df_load2["data"]).astype(df_dtypes2)
     print("復元されたDataFrame:")
     print(df_restore2)
     print(df_restore2.dtypes)
@@ -65,20 +65,20 @@ if __name__ == "__main__":
     print()
     assert_frame_equal(df, df_restore2)
 
-    df_dtypes = df.dtypes.apply(lambda x: x.name).to_dict()
-    df_dict = df.to_dict(orient="split")
-    df_str = json.dumps({"data": df_dict, "dtypes": df_dtypes})
+    df_dtypes3 = df.dtypes.apply(lambda x: x.name).to_dict()
+    df_dict3 = df.to_dict(orient="split")
+    df_str3 = json.dumps({"data": df_dict3, "dtypes": df_dtypes3})
     print("パターン3")
     print("json.dumpsの文字列:")
-    print(df_str)
-    print(f"サイズ: {sys.getsizeof(df_str)} Bytes")
+    print(df_str3)
+    print(f"サイズ: {sys.getsizeof(df_str3)} Bytes")
     print()
 
-    df_load = json.loads(df_str)
-    df_dtypes = {k: v for k, v in df_load["dtypes"].items()}
+    df_load3 = json.loads(df_str3)
+    df_dtypes3 = {k: v for k, v in df_load3["dtypes"].items()}
     df_restore3 = pd.DataFrame(
-        df_load["data"]["data"], columns=df_load["data"]["columns"]
-    ).astype(df_dtypes)
+        df_load3["data"]["data"], columns=df_load3["data"]["columns"]
+    ).astype(df_dtypes3)
     print("復元されたDataFrame:")
     print(df_restore3)
     print(df_restore3.dtypes)
@@ -89,8 +89,8 @@ if __name__ == "__main__":
     print("パターン4")
     # DataFrameをCSV形式の文字列に変換
     csv_string = df.to_csv(index=False)
-    df_dtypes = df.dtypes.apply(lambda x: x.name).to_dict()
-    str_data = json.dumps({"data": csv_string, "dtypes": df_dtypes})
+    df_dtypes4 = df.dtypes.apply(lambda x: x.name).to_dict()
+    str_data = json.dumps({"data": csv_string, "dtypes": df_dtypes4})
     print("CSV形式の文字列:")
     print(str_data)
     print(f"サイズ: {sys.getsizeof(str_data)} Bytes")
